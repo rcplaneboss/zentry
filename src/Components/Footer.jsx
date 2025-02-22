2402e52fcbf7c28a2ec1bdc95147b565deb5fd25
rcplaneboss <qosimabdulrahman0@gmail.com>
2025-02-22 20:01:56 +0100

    updated contact section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <Footer className="bg-violet-300 py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p> &copy; AXIOM 2024. All right preserved. </p>
+      </div>
+    </Footer>
   )
 }
 
