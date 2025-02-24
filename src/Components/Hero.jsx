784707d015c22e8d6090463fbb077f9c9bfc61ad
rcplaneboss <qosimabdulrahman0@gmail.com>
2025-02-24 12:34:31 +0100

    updated hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [loadedVideos, setLoadedVideos] = useState(0);
+  const [loadedVideos, setLoadedVideos] = useState(1);
 
   const totalVideos = 4;
 
